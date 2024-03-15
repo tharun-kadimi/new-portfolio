c57a3bdd5c30e253ca0400942d6c445071a5bbcd
tharun-kadimi <tharunkadimi13@gmail.com>
2024-03-15 11:23:55 +0530

    created a new portfolio website

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../assets/logo.png";
+import contactimg from "../assets/contact.png";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About Me</Link>
+        <Link className="desktopMenuListItem">Projects</Link>
+        <Link className="desktopMenuListItem">Resume</Link>
+      </div>
+      <button type="button" className="desktopMenuBtn">
+        <img src={contactimg} alt="" className="desktopMenuImg" /> Contact Me
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
